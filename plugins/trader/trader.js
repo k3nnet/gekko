5a5d45c3d43ed48e7ac950b45ba1a0011406f02a
Mike van Rossum <mike@mikevanrossum.nl>
2018-08-17 00:27:23 +0800

    ref correct trailValue prop

--- plugins/trader/trader.js
+++ plugins/trader/trader.js
@@ -339,7 +339,7 @@ Trader.prototype.createOrder = function(side, amount, advice, id) {
             initialPrice: this.price,
             type: 'trialingStop',
             properties: {
-              trail: advicetrigger.trailValue
+              trail: trigger.trailValue
             }
           });
 
